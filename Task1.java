e2fc9901f320d0db510b93d06dd6c623fb15d8e4
MikhailNorkin <mvnorkin@gmail.com>
2022-10-22 20:11:44 +0300

    Task1 is complete

---
+++ Task1.java
@@ -0,0 +1,16 @@
+//  Вычислить n-ое треугольного число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        java.util.Scanner iScanner = new Scanner (System.in);
+        System.out.print("Введите фактариал: ");
+        int n = iScanner.nextInt();
+        int fac = 1;
+        for (int i = 1; i <=n ; i++) {
+            fac = fac * i;
+        }
+        System.out.printf("Факториал %s равен: %s", n, fac);
+    }
+}
