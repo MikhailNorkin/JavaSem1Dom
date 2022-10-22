a49b4584001b2db6119cb99ce6f4a5ae72c96bba
MikhailNorkin <mvnorkin@gmail.com>
2022-10-23 01:17:05 +0300

    Task 3 is complete

--- Task3.java
+++ Task3.java
@@ -1,38 +1,45 @@
-// Реализовать простой калькулятор
+//Реализовать простой калькулятор
 
 import java.util.Scanner;
 
 public class Task3 {
 
-    public static int metod_input() {
-        java.util.Scanner iScanner = new Scanner (System.in);
-        System.out.print("Введите число: ");
-        int n = iScanner.nextInt();
-        iScanner.close();
-        return n;
-    }
-
-    public static String metod_expression() {
-        java.util.Scanner iScanner = new Scanner (System.in);
-        System.out.print("Введите выражение: ");
-        String n = iScanner.nextLine();
-        iScanner.close();
-        return n;
-    }
-    public void metod_close() {
-        java.util.Scanner iScanner = new Scanner (System.in);
-        iScanner.close();
-    }
-
     public static void main(String[] args) {
-        int a = metod_input();
-        String v = metod_expression();
-        int b = metod_input();
-        Task3 test = new Task3 ();
-        test.metod_close();
-        System.out.println(v);
-        if (v.equals(String.valueOf("+"))) {
-            System.out.printf("Сумма а и b равна %s\n", a+b);
-        }    
+
+        System.out.println("Введите через Enter первое число, вырожение и второе число, после чего выйдет итог:"); 
+
+        Scanner Sc = new Scanner(System.in);
+
+        System.out.println("Введите первое число: "); 
+        double a = Sc.nextInt();     
+
+        Scanner Sk = new Scanner(System.in);
+        System.out.println("Введите выражение: ");        
+        String expression = Sk.nextLine();
+
+        System.out.println("Введите второе число: "); 
+        double b = Sc.nextInt();
+
+        Sk.close();
+        Sc.close();
+
+        double res = 0;
+        if (expression.equals(String.valueOf("+"))){
+            res = a+b;    
+            System.out.printf("Сумма чисел %s и %s равна %s", a, b, res);
+        }
+        else if (expression.equals(String.valueOf("-"))){
+            res = a-b;
+            System.out.printf("Разница чисел %s и %s равна %s", a, b, res);
+        }
+        else if (expression.equals(String.valueOf("*"))){
+            res = a*b;    
+            System.out.printf("Произведение чисел %s и %s равно %s", a, b, res);
+        }
+        else if (expression.equals(String.valueOf("/"))){
+            res = a/b;    
+            System.out.printf("Деление чисел %s и %s равно %s", a, b, res);
+        }   
     }
-}
+}        
+
