c4e1f1d5bdde8ca601c8fd557608d6e193bf79f4
MikhailNorkin <mvnorkin@gmail.com>
2022-10-22 20:39:59 +0300

    Task1 and Task2 is complete

--- Task1.java
+++ Task1.java
@@ -5,13 +5,16 @@ import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
         java.util.Scanner iScanner = new Scanner (System.in);
-        System.out.print("Введите фактариал: ");
+        System.out.print("Введите число: ");
         int n = iScanner.nextInt();
         iScanner.close();
         int fac = 1;
+        int triangle = 0;
         for (int i = 1; i <=n ; i++) {
+            triangle = triangle + i;
             fac = fac * i;
         }
-        System.out.printf("Факториал %s равен: %s", n, fac);
+        System.out.printf("Треугольное число %s равено: %s\n", n, triangle);
+        System.out.printf("Факториал числа %s равен: %s", n, fac);
     }
 }
--- Task2.java
+++ Task2.java
@@ -2,7 +2,8 @@
 
 public class Task2 {
     public static void main(String[] args) {
-        for (int i = 2; i <= 100; i++) {
+        System.out.print("Простые числа от 1 до 1000: ");
+        for (int i = 2; i <= 1000; i++) {
             for (int j = 2; j <= i; j++) {
                 if (i % j == 0) {
                     if (i == j) {
